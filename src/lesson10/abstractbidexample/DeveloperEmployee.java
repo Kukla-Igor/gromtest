ed8995d8b5d04d213de258a00ff6918a7ae4002e
igor.kukla1992@gmail.com <170892igor>
2018-11-01 21:08:46 +0200

    add lesson10

---
+++ src/lesson10/abstractbidexample/DeveloperEmployee.java
@@ -0,0 +1,13 @@
+package lesson10.abstractbidexample;
+
+public class DeveloperEmployee extends Employee{
+
+    private String[] frameWorks = new String[10];
+
+    @Override
+    void paySalary() {
+        int newBalance = getBankAccount().getBalance() + getSalaryPerMonth() + 1000;
+        getBankAccount().setBalance(newBalance);
+
+    }
+}
---
+++ src/lesson10/abstractfirstexample/DbProvider.java
@@ -0,0 +1,16 @@
+package lesson10.abstractfirstexample;
+
+public abstract class DbProvider {
+
+    private String dbHost;
+
+
+
+    abstract void connectToDb();
+
+    abstract void disconnectFromDb();
+
+    void printDbHost() {
+        System.out.println("DB host is " + dbHost);
+    }
+}
