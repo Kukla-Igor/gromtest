6f3dc0a539afb2e7a97ceb9ec78c884de4284a84
igor.kukla1992@gmail.com <170892igor>
2018-11-03 16:53:15 +0200

    add lesson11

---
+++ src/lesson11/interfaceexample/PostgreSQLDbProvider.java
@@ -0,0 +1,17 @@
+package lesson11.interfaceexample;
+
+public class PostgreSQLDbProvider implements DbProvider {
+    @Override
+    public void connectToDb() {
+
+    }
+    @Override
+    public void disconnectFromDb() {
+
+    }
+
+    @Override
+    public void encryptData() {
+
+    }
+}
