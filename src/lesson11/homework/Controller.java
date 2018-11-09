541876f1acc91c96eef759329b48f60c29c4139f
igor.kukla1992@gmail.com <170892igor>
2018-11-09 21:14:18 +0200

    add lesson11.Homework

--- src/lesson11/homework/Controller.java
+++ src/lesson11/homework/Controller.java
@@ -29,9 +29,16 @@ public class Controller {
         Room[] rooms1 = new Room[api1.getAll().length];
         Room[] rooms2 = new Room[api2.getAll().length];
         int length = 0;
+        int big = rooms2.length;
+        int small = rooms1.length;
 
-        for (int i = 0; i < rooms1.length; i++){
-            for (int j = 0; j < rooms2.length; j++){
+        if (rooms1.length > rooms2.length) {
+            big = rooms1.length;
+            small = rooms2.length;
+        }
+
+        for (int i = 0; i < big; i++){
+            for (int j = 0; j < small; j++){
                 if (rooms1[i].getHotelName() == rooms2[j].getHotelName() && rooms1[i].getCityName() == rooms2[j].getCityName() && rooms1[i].getPersons() == rooms2[j].getPersons() && rooms1[i].getPrice() == rooms2[j].getPrice()){
                     length++;
                 }
@@ -42,8 +49,8 @@ public class Controller {
         int k = 0;
 
 
-        for (int i = 0; i < rooms1.length; i++){
-            for (int j = 0; j < rooms2.length; j++){
+        for (int i = 0; i < big; i++){
+            for (int j = 0; j < small; j++){
                 if (rooms1[i].getHotelName() == rooms2[j].getHotelName() && rooms1[i].getCityName() == rooms2[j].getCityName() && rooms1[i].getPersons() == rooms2[j].getPersons() && rooms1[i].getPrice() == rooms2[j].getPrice()){
                     allRooms[k] = rooms1[i];
                     k++;
