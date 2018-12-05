a5ca399095673186bb352f12e08959e7572cff56
igor.kukla1992@gmail.com <170892igor>
2018-12-05 14:20:26 +0200

    lesson 19

--- src/lesson19/homework/Controller.java
+++ src/lesson19/homework/Controller.java
@@ -11,39 +11,36 @@ public class Controller {
         try {
             formatCheck(storage, file);
         } catch (Exception e) {
-            System.out.println("формат файла " + file.getId() + " не поддерживаеться хранилищем " + storage.getId());
+            System.err.println("формат файла " + file.getId() + " не поддерживаеться хранилищем " + storage.getId());
             return;
         }
 
         try {
             sizeCheck(storage, file);
         } catch (Exception e) {
-            System.out.println("размер файла " + file.getId() + " не помещаеться в хранилище " + storage.getId());
+            System.err.println("размер файла " + file.getId() + " не помещаеться в хранилище " + storage.getId());
             return;
         }
 
         try {
             idCheck(storage, file);
         } catch (Exception e) {
-            System.out.println("файл " + file.getId() + " уже есть в хранилище " + storage.getId());
+            System.err.println("файл " + file.getId() + " уже есть в хранилище " + storage.getId());
             return;
         }
 
         try {
             nullCheck(storage, file);
         } catch (Exception e) {
-            System.out.println("для файла " + file.getId() + " нет свободных ячеек в хранилище " + storage.getId());
+            System.err.println("для файла " + file.getId() + " нет свободных ячеек в хранилище " + storage.getId());
             return;
         }
 
 
-        for (int i = 0; i < storage.getFiles().length; i++){
+        for (int i = 0; i < storage.getFiles().length; i++) {
             if (storage.getFiles()[i] == null)
                 storage.getFiles()[i] = file;
         }
-
-
-
     }
 
     private static void formatCheck(Storage storage, File file) throws Exception {
