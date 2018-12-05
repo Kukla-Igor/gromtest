36d80ffbda885f8775810b30b7eb23c9a3b563b3
igor.kukla1992@gmail.com <170892igor>
2018-12-05 14:11:15 +0200

    lesson 19

--- src/lesson19/homework/Controller.java
+++ src/lesson19/homework/Controller.java
@@ -5,10 +5,8 @@ import java.awt.image.RasterFormatException;
 public class Controller {
 
     public static void put(Storage storage, File file) {
-
         if (file == null)
-            throw new NullPointerException("Фвйл не указан");
-
+            throw new RuntimeException("Файл не указан");
 
         try {
             formatCheck(storage, file);
@@ -86,6 +84,7 @@ public class Controller {
         }
         throw new Exception("Size check error");
     }
+
 
 
 }
